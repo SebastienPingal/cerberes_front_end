908641bb71a1f5bd0717c5a5442f2c489903aba4
sebastien <sebastien.pingal@gmail.com>
2023-08-29 15:02:25 +0200

    feat(types): init

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,35 @@
 import { type ViteSSGContext } from 'vite-ssg'
 
+export interface IUser {
+  User_id: number
+  User_name: string
+  User_email: string
+  User_password?: string
+  User_contact_uuid?: string
+  PGP_PublicKey?: string
+}
+
+export interface IContact {
+  Contact_id: number
+  User_id: number
+  Contact_User_id: number
+}
+
+export interface IConversation {
+  Conversation_id: number
+}
+
+export interface IUserConversation {
+  UserConversation_id: number
+  User_id: number
+  Conversation_id: number
+}
+
+export interface IMessage {
+  Message_id: number
+  Conversation_id: number
+  Sender_id: number
+  Message_content: string
+}
+
 export type UserModule = (ctx: ViteSSGContext) => void
