72c9facd0a0d7fbf673450c406ac83a1545acbb6
sebastien <sebastien.pingal@gmail.com>
2023-08-30 14:01:52 +0200

    feat(header): link to profile

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
+const router = useRouter()
 const user_store = useUserStore()
 const user = computed(() => user_store.user)
+
+function go_to_profile() {
+  router.push('/profile')
+}
 </script>
 
 <template>
   <div class="header w-max-7xl w-full flex justify-between">
-    <img src="../assets/logo.png" alt="logo">
-    <h1 v-if="user">
+    <o-button v-if="user" rounded @click="go_to_profile">
       {{ user.User_name }}
-    </h1>
+    </o-button>
   </div>
 </template>
