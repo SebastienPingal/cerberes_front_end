8ffe2ca75d550fdf178b61cbdc73a16629262902
sebastien <sebastien.pingal@gmail.com>
2023-08-30 14:00:18 +0200

    feat(page): profile

---
+++ src/pages/profile.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+const router = useRouter()
+const user_store = useUserStore()
+const user = computed(() => user_store.user)
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-3">
+    <img src="../../Derperus.png" alt="Derperus" class="w-250px cursor-pointer" @click="router.push('/')">
+    <h1 class="text-2xl">
+      Profile
+    </h1>
+    <div v-if="user">
+      <div>
+        {{ user.User_name }}
+      </div>
+      <div>
+        {{ user.User_email }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
