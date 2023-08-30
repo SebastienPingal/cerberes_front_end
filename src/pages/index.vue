48c87102570935f6254cf5800a1ec5605dc818e3
sebastien <sebastien.pingal@gmail.com>
2023-08-30 14:07:47 +0200

    feat(index): deleteted useless, and polished

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,47 +1,23 @@
 <script setup lang="ts">
-import axios from 'axios'
-
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
-const api_url = import.meta.env.VITE_API_URL
 
+const display_store = useDisplayStore()
+const user_store = useUserStore()
+const user = computed(() => user_store.user)
 const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-async function me() {
-  await axios.get(`${api_url}/users/me`, { withCredentials: true })
-}
-
-const { t } = useI18n()
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-3">
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
+    <img src="../../Derperus.png" alt="Derperus" class="w-250px" @click="router.push('/')">
     <p>
-      <a rel="noreferrer" href="https://github.com/sebastienpingal/cerberes_front_end" target="_blank">
-        Cerberes
-      </a>
+      <a rel="noreferrer" href="https://github.com/sebastienpingal/cerberes_front_end" target="_blank"> Cerberes </a>
     </p>
-    <TheLogin />
-    <TheRegister />
-    <o-button @click="me">
-      Me
-    </o-button>
-    <TheInput v-model="name" :placeholder="t('intro.whats-your-name')" autocomplete="false" @keydown.enter="go" />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button m-3 text-sm btn :disabled="!name" @click="go">
-        {{ t('button.go') }}
-      </button>
+    <div v-if="!user">
+      <TheLogin v-if="display_store.login" />
+      <TheRegister v-if="display_store.register" />
     </div>
   </div>
 </template>
