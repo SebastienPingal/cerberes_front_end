ac65572f95f28a01b5e524c1d0001042356f9fc4
sebastien <sebastien.pingal@gmail.com>
2023-08-30 14:37:33 +0200

    fix(pipeline)

--- src/modules/Onu-uno.ts
+++ src/modules/Onu-uno.ts
@@ -2,6 +2,6 @@ import OnuUI from 'onu-ui'
 import { type UserModule } from '~/types'
 import 'onu-ui/dist/style.css'
 
-export const install: UserModule = ({ app, router, isClient }) => {
+export const install: UserModule = ({ app }) => {
   app.use(OnuUI)
 }
