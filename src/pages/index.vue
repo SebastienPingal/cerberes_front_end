f35ad771487ffef69dd849d916c0186cd5109f27
sebastien <sebastien.pingal@gmail.com>
2023-08-31 11:56:09 +0200

    test: env var

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,11 +7,13 @@ const display_store = useDisplayStore()
 const user_store = useUserStore()
 const user = computed(() => user_store.user)
 const router = useRouter()
+const test = import.meta.env.VITE_API_URL
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-3">
     <img src="../../Derperus.png" alt="Derperus" class="w-250px" @click="router.push('/')">
+    {{ test }}
     <p>
       <a rel="noreferrer" href="https://github.com/sebastienpingal/cerberes_front_end" target="_blank"> Cerberes </a>
     </p>
