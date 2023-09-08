203dd264bc60885a19384a65eee056aa889b72b9
sebastien <sebastien.pingal@gmail.com>
2023-09-08 05:09:52 +0200

    feat(encryption): export key

--- src/components/TheKey.vue
+++ src/components/TheKey.vue
@@ -1,74 +1,24 @@
 <script setup lang="ts">
 const encryption_store = useEncryptionStore()
+const utils_store = useUtilsStore()
 const display_generator = ref(false)
 const display_importator = ref(false)
 
 const signing_keypair = computed(() => encryption_store.signing_keypair)
 const encryption_keypair = computed(() => encryption_store.encryption_keypair)
-
-const lorem_ipsum = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies ultricies, nisl nisl ultricies nisl, nec ult')
-const encrypted_lorem_ipsum = ref(null) as Ref<{ nonce: Uint8Array; encryptedMessage: Uint8Array } | null>
-const decrypted_lorem_ipsum = ref(null) as Ref<string | null>
-
-function encrypt_lorem_ipsum() {
-  if (encryption_keypair?.value?.publicKey && signing_keypair?.value?.secretKey) {
-    const bob_encryption_keypair = encryption_store.generateEncryptionKeyPair()
-
-    const test = 'bonjour'
-    const signed = encryption_store.signMessage(test, signing_keypair.value.secretKey)
-    const verified = encryption_store.verifySignedMessage(signed, signing_keypair.value.publicKey)
-    console.log('signed', signed)
-    console.log('verified', verified)
-
-    // test just to encrypt and decript a message
-    const encrypted = encryption_store.encryptMessage(
-      test,
-      bob_encryption_keypair.publicKey,
-      encryption_keypair.value.secretKey,
-    )
-
-    const decrypted = encryption_store.decryptMessage(
-      encrypted,
-      encryption_keypair.value.publicKey,
-      bob_encryption_keypair.secretKey,
-    )
-    console.log('encrypted', encrypted)
-    console.log('decrypted', decrypted)
-
-    // test to encrypt and decript a message with signature
-
-    encrypted_lorem_ipsum.value = encryption_store.signAndEncryptMessage(
-      lorem_ipsum.value,
-      bob_encryption_keypair.publicKey,
-      encryption_keypair.value.secretKey,
-      signing_keypair.value.secretKey,
-    )
-
-    decrypted_lorem_ipsum.value = encryption_store.decryptAndVerifyMessage(
-      encrypted_lorem_ipsum.value,
-      signing_keypair.value.publicKey,
-      encryption_keypair.value.publicKey,
-      bob_encryption_keypair.secretKey,
-    )
-  }
-}
 </script>
 
 <template>
   <div>
-    <div v-if="signing_keypair?.secretKey" class="w-20rem">
+    <div v-if="signing_keypair?.secretKey && encryption_keypair?.secretKey" class="w-20rem">
       <p>You have a key</p>
-      <p>{{ lorem_ipsum }}</p>
-      <o-button @click="encrypt_lorem_ipsum">
-        Encrypt
-      </o-button>
-      <p v-if="encrypted_lorem_ipsum" class="truncated text-red">
-        {{ encrypted_lorem_ipsum }}
-      </p>
-      <p v-if="decrypted_lorem_ipsum" class="truncated text-blue">
-        {{ decrypted_lorem_ipsum }}
-      </p>
+      <div class="flex justify-around gap-3">
+        <o-button @click="utils_store.download_keys">
+          Export it
+        </o-button>
+      </div>
     </div>
+
     <div v-else class="flex flex-col gap-2">
       <div v-if="!display_generator && !display_importator">
         <p>You don't have any key yet.</p>
@@ -81,6 +31,7 @@ function encrypt_lorem_ipsum() {
           </o-button>
         </div>
       </div>
+
       <div v-else class="flex justify-center">
         <o-button @click="display_generator = false; display_importator = false">
           Change your mind
--- src/stores/utils.ts
+++ src/stores/utils.ts
@@ -1,11 +1,56 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 
-export const useUtilsStore = defineStore('user', () => {
-  function check_connection() {
-    return document.cookie.split(';').some(item => item.trim().startsWith('authToken='))
+export const useUtilsStore = defineStore('utils', () => {
+  function uint8ArrayToBase64(buffer: Uint8Array): string {
+    return btoa(String.fromCharCode(...buffer))
   }
+
+  // a function that kinda looks like a boat.
+  // I mean, look at the shape of it
+  // And the color
+  // this comment is like the smoke
+  // and the function is the boat
+  // it is sailing away
+  // man!
+  // it is an aircraft carrier !
+  // wow
+  function serializedKeys(
+    encryption_key_pair: {
+      publicKey: Uint8Array
+      secretKey: Uint8Array
+    },
+    signing_key_pair: {
+      publicKey: Uint8Array
+      secretKey: Uint8Array
+    }) {
+    return {
+      encryptionPublicKey: uint8ArrayToBase64(encryption_key_pair.publicKey as Uint8Array),
+      encryptionSecretKey: uint8ArrayToBase64(encryption_key_pair.secretKey as Uint8Array),
+      signingPublicKey: uint8ArrayToBase64(signing_key_pair.publicKey as Uint8Array),
+      signingSecretKey: uint8ArrayToBase64(signing_key_pair.secretKey as Uint8Array),
+    }
+  }
+
+  const JSonKeys = JSON.stringify(serializedKeys)
+
+  function download_keys() {
+    const blob = new Blob([JSonKeys], { type: 'text/plain' })
+    const blobURL = window.URL.createObjectURL(blob)
+    const tempLink = document.createElement('a')
+    tempLink.style.display = 'none'
+    tempLink.href = blobURL
+    tempLink.setAttribute('download', 'keys.json')
+
+    document.body.appendChild(tempLink)
+    tempLink.click()
+    document.body.removeChild(tempLink)
+    window.URL.revokeObjectURL(blobURL)
+
+    return true
+  }
+
   return {
-    check_connection,
+    download_keys,
   }
 })
 
