654f0c278d5719c2c111ae3c5a4140a7228c2beb
sebastien <sebastien.pingal@gmail.com>
2023-09-23 13:14:46 +0200

    style(TheConversation)

--- src/components/TheConversation.vue
+++ src/components/TheConversation.vue
@@ -7,7 +7,16 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>
-    {{ props.conversation.Users }}
+  <div class="btn-contact place-self-start">
+    {{ props.conversation.Users[0].User_name }}
   </div>
 </template>
+
+<style scoped>
+.btn-contact {
+  @apply w-xs flex place-content-center border rounded p-2 cursor-pointer
+}
+.btn-contact:hover {
+  @apply bg-gray-800
+}
+</style>
