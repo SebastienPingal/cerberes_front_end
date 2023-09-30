734359d173d400560d038a765878c639274e1b8c
sebastien <sebastien.pingal@gmail.com>
2023-09-30 18:44:08 +0200

    feat(TheMessageList): autmaticaly scroll down to see last messages

--- src/components/TheMessageList.vue
+++ src/components/TheMessageList.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 const conversation_store = useConversationStore()
+const message_container = ref<HTMLDivElement | null>(null)
 
+watch(() => conversation_store.selected_conversation?.Messages, async () => {
+  await nextTick()
+  // Add a small delay before scrolling
+  setTimeout(() => {
+    if (message_container.value)
+      message_container.value.scrollTop = message_container.value.scrollHeight
+  }, 100)
+}, { immediate: true })
+
+onMounted(async () => {
+  await nextTick()
+  // Add a small delay before scrolling
+  setTimeout(() => {
+    if (message_container.value)
+      message_container.value.scrollTop = message_container.value.scrollHeight
+  }, 100)
+})
 </script>
 
 <template>
   <div class="border p-4 overflow-hidden rounded-md flex flex-col" style="height: calc(100vh - 200px)">
     <div v-if="conversation_store.selected_conversation" class="flex flex-col h-full">
       <div v-if="conversation_store.selected_conversation?.Messages" class="flex flex-col gap-4 overflow-auto mb-4"
-        style="flex: 1">
+        ref="message_container" style="flex: 1">
         <div class="px-10 flex flex-col gap-4">
           <TheMessage v-for="message in conversation_store.selected_conversation.Messages" :key="message.Message_id"
             :message="message" />
