79cbaabdbc36feecd4e36b956ba7f0ba3991bcc1
sebastien <sebastien.pingal@gmail.com>
2023-10-04 11:44:56 +0200

    style(ContactList): style and displays

--- src/components/TheContactList.vue
+++ src/components/TheContactList.vue
@@ -25,11 +25,11 @@ function add_contact() {
     </div>
     <div v-if="display_add_contact" class="flex flex-col gap-1 border border-green p-2">
       <input v-model="input_contact" placeholder="Enter uuid here" class="pl-2" @keypress.enter="add_contact">
-      <o-button @click="add_contact">
+      <o-button @click="add_contact" type="success" light>
         Ajouter un contact
       </o-button>
     </div>
-    <div v-else-if="user.contact_list.length > 1">
+    <div v-else-if="user.contact_list.length">
       <o-button type="success" light class="text-xl" @click="display_add_contact = true">
         +
       </o-button>
